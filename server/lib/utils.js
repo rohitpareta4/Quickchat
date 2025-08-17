6be0576cd997c7c63b8a0d3f4b43f622864121e2
rohitpareta4 <rockk7x@gmail.com>
2025-08-17 11:26:13 +0530

    doneee

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -6,8 +6,10 @@ export const generatetoken=(userId,res)=>{
   res.cookie("jwt",token,{
     maxAge:7*24*60*60*1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure: process.env.NODE_ENV !== "development",
+    // sameSite:"strict",
+    sameSite:"none",
+    // secure: process.env.NODE_ENV !== "development",
+    secure:true
   })
   console.log("token",token)
   return token;
